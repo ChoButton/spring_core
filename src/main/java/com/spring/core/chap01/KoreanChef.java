17979b2dbc52a877ae0b346234cd9901801054bb
bitcamp1\bitcamp <chosy0716@gmail.com>
2023-05-22 17:52:47 +0900

    자바 스프링 1일차

---
+++ src/main/java/com/spring/core/chap01/KoreanChef.java
@@ -0,0 +1,9 @@
+package com.spring.core.chap01;
+
+public class KoreanChef implements Chef{
+    @Override
+    public void cook() {
+        // sout : 프린트ln 자동생성
+        System.out.println("한식 마스터 김한식 입니다.");
+    }
+}
